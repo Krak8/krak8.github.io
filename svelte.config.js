d2456240fe053591c256486f57281486af447926
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2023-06-28 19:48:16 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
 		adapter: adapter(),
 		paths: {
